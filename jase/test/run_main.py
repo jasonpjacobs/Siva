ada3c32b40c6aa5963ada38e10eff18726516241
jasonpjacobs <jason.jacobs@gmail.com>
2014-11-06 11:18:35 -0800

    Cleanup of run_main

--- jase/test/run_main.py
+++ jase/test/run_main.py
@@ -1,18 +1,10 @@
-import pytest
 import sys
-
-from PySide import  QtCore, QtGui, QtTest
-from jase.schematic.schematic import Schematic
 from jase.ui.main import Main
 from jase.ui.app import App
 app = App()
-
 
 if __name__ == "__main__":
     m = Main()
     m.setGeometry(100, 100, 1200, 800)
     m.show()
-
-    m.status('Ready.')
-
     sys.exit(app.exec_())
