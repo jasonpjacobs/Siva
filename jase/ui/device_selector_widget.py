b8cb499a5512f17b029f46082c157b1cc007d8af
jasonpjacobs <jason.jacobs@gmail.com>
2014-11-17 21:51:45 -0800

    Devise selector widget now uses ListView, ListModel

--- jase/ui/device_selector_widget.py
+++ jase/ui/device_selector_widget.py
@@ -1,9 +1,10 @@
 from PySide import QtCore, QtGui
 
-from jtypes.table_model import TableModel, TableView
+from jtypes.list_model import ListModel, ListView
 
-class DeviceCanvas(TableView):
-    pass
+class DeviceCanvas(ListView):
+    def __init__(self, data=None):
+        super().__init__(columns=['name'])
 
 class DeviceSelectorWidget(QtGui.QWidget):
     '''
@@ -17,6 +18,7 @@ class DeviceSelectorWidget(QtGui.QWidget):
 
         self.libLabel = QtGui.QLabel("Library")
         self.libEntry = QtGui.QComboBox()
+        self.libEntry.setEditable(True)
         self.cellLabel = QtGui.QLabel("Cell Filter")
         self.cellEntry = QtGui.QLineEdit("*")
         self.viewLabel = QtGui.QLabel("Views")
@@ -24,30 +26,25 @@ class DeviceSelectorWidget(QtGui.QWidget):
         self.collectionLabel = QtGui.QLabel("Collections")
         self.collectionEntry = QtGui.QLineEdit("*")
 
-        self.deviceLabel = QtGui.QLabel("Devices")
+        self.deviceLabel = QtGui.QLabel("Cells")
         self.deviceCanvas = DeviceCanvas()
 
         # Configure the device canvas
         self.deviceCanvas.setIconSize(QtCore.QSize(100,100))
         self.deviceCanvas.setDragDropMode(QtGui.QAbstractItemView.DragOnly)
-        #self.deviceCanvas.setView
+        #self.deviceCanvas.setViewMode(QtGui.QListView.IconMode)
 
-        self.setGeometry(300, 300, 350, 250)
+        #self.setGeometry(300, 300, 350, 250)
 
         grid = QtGui.QGridLayout()
-        #grid.addWidget(self.collectionLabel, 0, 0)
-        #grid.addWidget(self.collectionEntry, 0, 1)
-        grid.addWidget(self.libLabel, 1, 0)
-        grid.addWidget(self.libEntry, 1, 1)
+        grid.addWidget(self.libLabel, 0, 0)
+        grid.addWidget(self.libEntry, 0, 1)
 
-        grid.addWidget(self.cellLabel, 2,0)
-        grid.addWidget(self.cellEntry,2,1)
+        grid.addWidget(self.cellLabel, 1,0)
+        grid.addWidget(self.cellEntry,1,1)
 
-        #grid.addWidget(self.viewLabel, 3 ,0)
-        #grid.addWidget(self.viewEntry,3,1)
-
-        grid.addWidget(self.deviceLabel,4,0)
-        grid.addWidget(self.deviceCanvas,5,0,9,2)
+        grid.addWidget(self.deviceLabel,2,0)
+        grid.addWidget(self.deviceCanvas,3,0,9,2)
 
         layout = QtGui.QVBoxLayout()
         layout.addLayout(grid)
@@ -60,20 +57,24 @@ class DeviceSelectorWidget(QtGui.QWidget):
         # Signals/slots
         self.libEntry.currentIndexChanged.connect(self.onLibChange)
 
+        policy = QtGui.QSizePolicy()
+        policy.setHorizontalPolicy(QtGui.QSizePolicy.Minimum)
+        policy.setVerticalPolicy(QtGui.QSizePolicy.Expanding)
+        self.setSizePolicy(policy)
+
+    def sizeHint(self, *args, **kwargs):
+        return QtCore.QSize(10,600)
+
     def populateLib(self):
         for lib in self.lib_defs:
             self.libEntry.addItem(lib)
-        model = TableModel(data=[], columns=['name'])
+        model = ListModel(data=[], columns=['name'])
         self.deviceCanvas.setModel(model)
         self.onLibChange()
 
     def onLibChange(self):
         lib_name = str(self.libEntry.currentText())
-
         assert lib_name in self.lib_defs
-
         lib = self.lib_defs[lib_name]
-        cell_names = [c for c in lib]
-        cell_names.sort()
-        print("New cells are", list(lib.values()))
         self.deviceCanvas.set_model_data(list(lib.values()))
+
