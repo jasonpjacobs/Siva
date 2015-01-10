802befe76f2a4ff3ee47faa9ea9118d7cca19859
jasonpjacobs <jason.jacobs@gmail.com>
2015-01-09 16:05:26 -0800

    Renamed Design class to Component class.

--- jase/__init__.py
+++ jase/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.0.1"
 from .ui.app import App
 from .ui.main import Main
-from .design import Design, Library, Cell, View
+from .design import Component, Library, Cell, View
 from .views import Symbol
 
