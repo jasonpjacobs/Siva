b38451e46178a69ef7db67136f891c5742572532
jasonpjacobs <jason.jacobs@gmail.com>
2015-01-13 13:32:24 -0800

    Code cleanup

--- jase/__init__.py
+++ jase/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.0.1"
 from .ui.app import App
 from .ui.main import Main
-from .design import Component, Library, Cell, View
+from .design import Library, Cell, View
 from .views import Symbol
 
