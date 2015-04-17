b510cda1578a2ea09fb9406c3fdb4a6e91dc38d6
jasonpjacobs <jason.jacobs@gmail.com>
2015-04-17 08:43:23 -0700

    Revert "Revert "Bug fixes in Simulation class.""
    This reverts commit 63b0ffa0e55003af24cf95e876e1ddc912a50a60.

--- jase/simulation/table.py
+++ jase/simulation/table.py
@@ -161,13 +161,20 @@ class Table:
             return self.save_as_hdf5(file_name=file_name, dir=dir, **kwargs)
 
 
-    def save_as_hdf5(self, file_name, dir=".", group=None):
+    def save_as_hdf5(self, file_name, dir=".", group=None, mode='w-'):
         import h5py
         import numpy as np
 
         path = os.path.join(dir, file_name)
 
-        fp = h5py.File(path, 'w-')
+        try:
+            fp = h5py.File(path, mode)
+        except OSError as e:
+            print("Error creating HDF5 file")
+            from jase.utilities.file_handles import list_file_handles
+            print(list_file_handles())
+            raise
+
 
         if group is None:
             ds = fp
@@ -184,8 +191,8 @@ class Table:
                 # See http://docs.h5py.org/en/latest/strings.html#exceptions-for-python-3 for more info
                 if type(data[0]) is str:
                     ds[col] = np.string_(data)
-        fp.flush()
-        return fp
+        fp.close()
+        return True
 
 
     def sort(self, column):
