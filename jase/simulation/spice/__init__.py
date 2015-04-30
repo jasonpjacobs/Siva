32b5c4ecfd103f0d2bd33e6f9f2f11d50e00232c
jasonpjacobs <jason.jacobs@gmail.com>
2015-04-30 15:45:46 -0700

    Added ideal current source

--- jase/simulation/spice/__init__.py
+++ jase/simulation/spice/__init__.py
@@ -6,7 +6,7 @@ from .option import Option
 from .analyses import Tran, DC, AC
 from .connections import Pin, Net, Input, Output, Global, GND
 from .primitives import Primitive, Pmos, Nmos, R, C, L
-from .sources import Vdc, Vpulse, Vpwl
+from .sources import Vdc, Vpulse, Vpwl, Idc
 from .save import Save, V, I
 from .circuit import Circuit
 from .simulation import Simulation
